8feecb96bfec4d59c372801cc4be56246509ad3d
AKIMITSU Taichi <aki32.png@gmail.com>
2022-11-15 13:18:12 +0900

    added SaveScreenShot

---
+++ A - Extensions (method)/E - Image/E003 SaveScreenShot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace Aki32_Utilities.Extensions;
+public static partial class OwesomeExtensions
+{
+
+    /// <summary>
+    /// SaveScreenShot
+    /// </summary>
+    /// <param name="outputFile">when null, automatically set to {inputDir.FullName}/output.pdf</param>
+    /// <returns></returns>
+    public static FileInfo SaveScreenShot(FileInfo outputFile)
+    {
+        throw new NotImplementedException();
+
+
+        // main
+
+
+        // post process
+        return outputFile;
+    }
+
+}
