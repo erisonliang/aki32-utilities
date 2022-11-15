95dd3c9000636fea71b86245e920145196dd3f95
AKIMITSU Taichi <aki32.png@gmail.com>
2022-11-15 11:30:07 +0900

    adding Books2PDF

--- MiniApps.cs
+++ MiniApps.cs
@@ -15,6 +15,55 @@ public class MiniApps
     /// </summary>
     public static void Books2PDF()
     {
-       
+        var lastPosition = new PointF(0, 0);
+
+        Console.WriteLine("★★★★★★★★★★★★★★★ 定義");
+
+        Console.WriteLine("※未実装");
+
+
+        Console.WriteLine("\r\n★★★★★★★★★★★★★★★ 座標入力\r\n");
+
+        var reasons = new string[] {
+            "撮影対象の左上",
+            "撮影対象の右上",
+            "先送りボタン",
+        };
+        var ps = new PointF[reasons.Length];
+
+        for (int i = 0; i < reasons.Length; i++)
+        {
+            Console.WriteLine($"\r\n{reasons[i]} の位置にカーソルを合わせて、Enterを押してください。");
+
+            Console.CursorVisible = false;
+            while (!Console.KeyAvailable)
+            {
+                lastPosition = IODeviceExtension.GetMouseCursorPosition();
+                ConsoleExtension.ClearCurrentConsoleLine();
+                Console.Write(lastPosition);
+                Thread.Sleep(10);
+            }
+            Console.CursorVisible = true;
+            Console.ReadLine();
+            ps[i] = lastPosition;
+        }
+
+        Console.WriteLine();
+        for (int i = 0; i < reasons.Length; i++)
+            Console.WriteLine($"{reasons[i]}:{ps[i]}");
+
+
+        Console.WriteLine("\r\n★★★★★★★★★★★★★★★ 撮影\r\n");
+
+        Console.WriteLine("※未実装");
+
+
+        Console.WriteLine("\r\n★★★★★★★★★★★★★★★ PDF化\r\n");
+
+        Console.WriteLine("※未実装");
+
+
+        Console.WriteLine("\r\n★★★★★★★★★★★★★★★ 以上\r\n");
+
     }
 }
