f07c8fc1866a39c838d2bf02a3dae72f659add64
aki32 <aki32.png@gmail.com>
2022-11-19 14:30:18 +0900

    add proportion target

--- 1 Console App Utilities/A - Extensions/E - Image/E006 ResizeImage.cs
+++ 1 Console App Utilities/A - Extensions/E - Image/E006 ResizeImage.cs
@@ -66,6 +66,52 @@ public static partial class OwesomeExtensions
         return outputDir!;
     }
 
+    /// <summary>
+    /// ResizeImage Proportionally
+    /// </summary>
+    /// <param name="inputFile"></param>
+    /// <param name="outputFile">when null, automatically set to {inputFile.DirectoryName}/output_ResizeImage/{inputFile.Name} - {crop.ToString()}.png</param>
+    /// <param name="outputSizeRatio">1 for the same size. More than 0. </param>
+    /// <returns></returns>
+    public static FileInfo ResizeImagePropotionally(this FileInfo inputFile, FileInfo? outputFile, SizeF outputSizeRatio,
+        ResizeImageMode mode = ResizeImageMode.Stretch)
+    {
+        // sugar
+        using var inputImage = inputFile.GetImageFromFile();
+        var outputSize = new Size(
+            (int)(inputImage.Width * outputSizeRatio.Width),
+            (int)(inputImage.Height * outputSizeRatio.Height));
+
+        return inputFile.ResizeImage(outputFile, outputSize, mode);
+
+    }
+
+    /// <summary>
+    /// ResizeImage Proportionally
+    /// </summary>
+    /// <param name="inputDir"></param>
+    /// <param name="outputDir">when null, automatically set to {inputDir.Fullname}/output_CropImage/{inputFile.Name}.png</param>
+    /// <param name="outputSizeRatio">1 for the same size. More than 0. </param>
+    /// <returns></returns>
+    public static DirectoryInfo ResizeImagePropotionally_Loop(this DirectoryInfo inputDir, DirectoryInfo? outputDir, SizeF outputSizeRatio,
+        ResizeImageMode mode = ResizeImageMode.Stretch)
+    {
+        // preprocess
+        UtilPreprocessors.PreprocessOutDir(ref outputDir, true, inputDir!);
+
+
+        // main
+        foreach (var inputFile in inputDir.GetFiles())
+        {
+            var outputFile = new FileInfo(Path.Combine(outputDir!.FullName, inputFile.Name));
+            inputFile.ResizeImagePropotionally(outputFile, outputSizeRatio, mode);
+        }
+
+
+        // post process
+        return outputDir!;
+    }
+
 
     // ★★★★★★★★★★★★★★★ Image process
 
