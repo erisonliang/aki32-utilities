36cfbe62260a28543f80a8f13fe71283b0c4c130
aki32 <aki32.png@gmail.com>
2022-11-23 17:28:55 +0900

    updated ClearCurrentConsoleLine method

--- 1 Console App Utilities/A - Extensions/W - Class/W201 ConsoleExtension.cs
+++ 1 Console App Utilities/A - Extensions/W - Class/W201 ConsoleExtension.cs
@@ -10,7 +10,8 @@ public static class ConsoleExtension
     {
         int currentLineCursor = Console.CursorTop;
         Console.SetCursorPosition(0, Console.CursorTop);
-        Console.Write(new string(' ', currentLineCursor)); //Console.WindowWidth
+        //Console.Write(new string(' ', currentLineCursor));
+        Console.Write(new string(' ', Console.WindowWidth));
         Console.SetCursorPosition(0, currentLineCursor);
     }
 
