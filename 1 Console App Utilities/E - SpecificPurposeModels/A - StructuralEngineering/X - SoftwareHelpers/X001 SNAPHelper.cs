4e7691dee8353408bbfbd9e07563e92e3c861278
AKIMITSU Taichi <aki32.png@gmail.com>
2022-12-06 16:14:17 +0900

    updated keep closing excel

--- 1 Console App Utilities/E - SpecificPurposeModels/A - StructuralEngineering/X - SoftwareHelpers/X001 SNAPHelper.cs
+++ 1 Console App Utilities/E - SpecificPurposeModels/A - StructuralEngineering/X - SoftwareHelpers/X001 SNAPHelper.cs
@@ -26,31 +26,37 @@ public static class SNAPHelper
         {
             Thread.Sleep(interval);
             Console.WriteLine("監視中…");
-
-            try
-            {
-                var ps = Process.GetProcessesByName("EXCEL");
-
-                if (ps.Length == 0)
-                    continue;
-
-                ConsoleExtension.WriteLineWithColor($"プロセス発見！ 個数 = {ps.Length}", ConsoleColor.Blue);
-                foreach (var p in ps)
-                {
-                    p.CloseMainWindow();
-                    p.WaitForExit(MAX_EXIT_WAITING_TIME);
-                    if (p.HasExited)
-                        ConsoleExtension.WriteLineWithColor($"プロセス終了！ ID = {p.Id}, 累計キル回数 = {++TotalKillCount}", ConsoleColor.Blue);
-                }
-            }
-            catch (Exception ex)
-            {
-                ConsoleExtension.WriteLineWithColor($"一時エラー：{ex}", ConsoleColor.Red);
-            }
+            CheckAndCloseExcelOnce();
         }
 
     }
+    public static bool CheckAndCloseExcelOnce()
+    {
+        try
+        {
+            var ps = Process.GetProcessesByName("EXCEL");
 
+            if (ps.Length == 0)
+                return false;
+
+            ConsoleExtension.WriteLineWithColor($"プロセス発見！ 個数 = {ps.Length}", ConsoleColor.Blue);
+            foreach (var p in ps)
+            {
+                p.CloseMainWindow();
+                p.WaitForExit(MAX_EXIT_WAITING_TIME);
+                if (p.HasExited)
+                    ConsoleExtension.WriteLineWithColor($"プロセス終了！ ID = {p.Id}, 累計キル回数 = {++TotalKillCount}", ConsoleColor.Blue);
+            }
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            ConsoleExtension.WriteLineWithColor($"一時エラー：{ex}", ConsoleColor.Red);
+        }
+
+        return false;
+    }
 
     // ★★★★★★★★★★★★★★★ 
 
