6711f806cec70652cd891a3784b9a8e51a5e3800
AKIMITSU Taichi <aki32.png@gmail.com>
2023-01-11 16:10:41 +0900

    added

---
+++ 1 Console/100 General/A - ChainableExtensions/_ - Base/_102 UtilTestClass2.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace Aki32Utilities.ConsoleAppUtilities.General;
+[JsonObject]
+public class UtilTestClass2
+{
+    [JsonProperty("age")]
+    public int Age { get; private set; }
+
+    [JsonProperty("name")]
+    public string Name { get; private set; }
+
+    public UtilTestClass2(int age, string name)
+    {
+        Age = age;
+        Name = name;
+    }
+}
