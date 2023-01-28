b9453da3f0634ee96e7292a8425e0100e8bc4b41
AKIMITSU Taichi <aki32.png@gmail.com>
2023-01-28 14:49:02 +0900

    added

---
+++ 1 Console/101 ML/A - ChainableExtensions/_  - Base/_001 Base.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Aki32Utilities.ConsoleAppUtilities.MachineLearning;
+public static partial class ChainableExtensions
+{
+    private const string SUB_REPO_NAME = "@Local - SubRepo";
+    
+    // not implemented yet
+
+}
