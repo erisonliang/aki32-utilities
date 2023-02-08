f48d22f8780e0be14ce1f042738784ccac70e8eb
AKIMITSU Taichi <aki32.png@gmail.com>
2023-02-08 16:07:32 +0900

    added GetFilesWithRegexen

--- 1 Console/100 General/A - ChainableExtensions/B - FileSystem/B101 GetFilesWithRegexen.cs
+++ 1 Console/100 General/A - ChainableExtensions/B - FileSystem/B101 GetFilesWithRegexen.cs
@@ -1,8 +1,19 @@
-﻿using System.IO.Compression;
+﻿
 
 namespace Aki32Utilities.ConsoleAppUtilities.General;
 public static partial class ChainableExtensions
 {
+    /// <summary>
+    /// Return if string match any of search regexen in current directory
+    /// </summary>
+    /// <param name="inputDir"></param>
+    /// <param name="outputFile">when null, automatically set</param>
+    /// <returns></returns>
+    public static IEnumerable<FileInfo> GetFilesWithRegexen(this DirectoryInfo inputDir, params string[] searchRegexen)
+    {
+        // main
+        return inputDir.GetFilesWithRegexen(SearchOption.TopDirectoryOnly, searchRegexen);
+    }
 
     /// <summary>
     /// Return if string match any of search regexen
