a3054bd74f30b4d759802ae4e92a25905416f966
aki32 <aki32.png@gmail.com>
2023-02-10 15:57:26 +0900

    added

---
+++ 1 Console/100 General/A - ChainableExtensions/E - Image/E103 ShowImage.cs
@@ -0,0 +1,50 @@
+﻿using OpenCvSharp;
+using Aki32Utilities.ConsoleAppUtilities.General;
+using System.Runtime.CompilerServices;
+using DocumentFormat.OpenXml.ExtendedProperties;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+using OpenCvSharp.XImgProc;
+
+namespace Aki32Utilities.ConsoleAppUtilities.General;
+public static partial class ChainableExtensions
+{
+
+    // ★★★★★★★★★★★★★★★ FileSystemInfo chain process
+
+    /// <summary>
+    /// sugar
+    /// play video file on your default application
+    /// </summary>
+    /// <param name="inputFile"></param>
+    public static void ShowImage_OnDefaultApp(this FileInfo inputFile, bool waitForExit = true)
+    {
+        // main
+        inputFile.OpenOnDefaultApp(waitForExit);
+    }
+
+    /// <summary>
+    /// Show image on OpenCV2 window.
+    /// </summary>
+    /// <param name="inputFile"></param>
+    /// <param name="outputDir">when null, automatically set</param>
+    /// <param name="capturingFrameRate">Capture only entered number of images per second.</param>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    /// <exception cref="InvalidDataException"></exception>
+    public static void ShowImage_OnThread(this FileInfo inputFile, bool waitForExit = false)
+    {
+        // main
+        var windowName = inputFile.Name; //"ShowImage";
+        using var window = new Window(windowName);
+        using var image = new Mat(inputFile.FullName);
+
+        if (waitForExit)
+            while (!IsCv2WindowClosed(windowName))
+                Task.Delay(200).Wait();
+
+    }
+
+
+    // ★★★★★★★★★★★★★★★ 
+
+}
