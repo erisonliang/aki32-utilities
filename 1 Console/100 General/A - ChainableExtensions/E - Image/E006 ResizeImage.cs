425d87bba24d8e2d962bf0373c24133864494a79
aki32 <aki32.png@gmail.com>
2023-02-10 18:00:34 +0900

    updated strategy

--- 1 Console/100 General/A - ChainableExtensions/E - Image/E006 ResizeImage.cs
+++ 1 Console/100 General/A - ChainableExtensions/E - Image/E006 ResizeImage.cs
@@ -97,38 +97,40 @@ public static partial class ChainableExtensions
         {
             case ResizeImageMode.Stretch:
                 {
-                    using var tempBitmap = new Bitmap(inputImage.Width + 1, inputImage.Height + 1);
-                    {
-                        var destination = new Rectangle(1, 1, inputImage.Width, inputImage.Height);
-                        using var g = Graphics.FromImage(tempBitmap);
-                        g.DrawImage(inputImage, destination, 0, 0, inputImage.Width, inputImage.Height, GraphicsUnit.Pixel);
-                    }
-
                     using var outputBitmap = new Bitmap(outputSize.Width, outputSize.Height);
-                    {
-                        var offsetX = outputSize.Width / inputImage.Width;
-                        var offsetY = outputSize.Height / inputImage.Height;
-                        var destination = new Rectangle(-offsetX / 2, -offsetY / 2, outputBitmap.Width + offsetX, outputBitmap.Height + offsetY);
 
-                        using var g = Graphics.FromImage(outputBitmap);
-                        g.InterpolationMode = interpolationMode;
-                        g.DrawImage(tempBitmap, destination, 0, 0, tempBitmap.Width, tempBitmap.Height, GraphicsUnit.Pixel);
+                    if (outputSize.Width <= inputImage.Width && outputSize.Height <= inputImage.Height)
+                    {
+                        // when make it smaller, use normal way.
+                        // (!) 1セル目を拡大するときに半分のサイズになっておかしい。だから拡大には使わない。
+                        {
+                            var attributes = new ImageAttributes();
+                            attributes.SetWrapMode(WrapMode.TileFlipXY);
+                            var destination = new Rectangle(0, 0, outputBitmap.Width, outputBitmap.Height);
+
+                            using var g = Graphics.FromImage(outputBitmap);
+                            g.InterpolationMode = interpolationMode;
+                            g.DrawImage(inputImage, destination, 0, 0, inputImage.Width, inputImage.Height, GraphicsUnit.Pixel, attributes);
+                        }
                     }
-
-                    return (Image)outputBitmap.Clone();
-                }
-            case ResizeImageMode.Stretch_Faster:
-                {
-                    // 元々の方法。でも1セル目を拡大するときに半分のサイズになっておかしい。
-                    using var outputBitmap = new Bitmap(outputSize.Width, outputSize.Height);
+                    else
                     {
-                        var attributes = new ImageAttributes();
-                        attributes.SetWrapMode(WrapMode.TileFlipXY);
-                        var destination = new Rectangle(0, 0, outputBitmap.Width, outputBitmap.Height);
+                        // when make it bigger, use special way
+                        using var tempBitmap = new Bitmap(inputImage.Width + 1, inputImage.Height + 1);
+                        {
+                            var destination = new Rectangle(1, 1, inputImage.Width, inputImage.Height);
+                            using var g = Graphics.FromImage(tempBitmap);
+                            g.DrawImage(inputImage, destination, 0, 0, inputImage.Width, inputImage.Height, GraphicsUnit.Pixel);
+                        }
+                        {
+                            var offsetX = outputSize.Width / inputImage.Width;
+                            var offsetY = outputSize.Height / inputImage.Height;
+                            var destination = new Rectangle(-offsetX / 2, -offsetY / 2, outputBitmap.Width + offsetX, outputBitmap.Height + offsetY);
 
-                        using var g = Graphics.FromImage(outputBitmap);
-                        g.InterpolationMode = interpolationMode;
-                        g.DrawImage(inputImage, destination, 0, 0, inputImage.Width, inputImage.Height, GraphicsUnit.Pixel, attributes);
+                            using var g = Graphics.FromImage(outputBitmap);
+                            g.InterpolationMode = interpolationMode;
+                            g.DrawImage(tempBitmap, destination, 0, 0, tempBitmap.Width, tempBitmap.Height, GraphicsUnit.Pixel);
+                        }
                     }
 
                     return (Image)outputBitmap.Clone();
@@ -156,7 +158,6 @@ public static partial class ChainableExtensions
     public enum ResizeImageMode
     {
         Stretch,
-        Stretch_Faster,
         Uniform_Notimplemented,
         UniformToFill_Notimplemented,
     }
