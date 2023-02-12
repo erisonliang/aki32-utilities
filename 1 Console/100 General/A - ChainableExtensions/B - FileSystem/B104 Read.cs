6101733047b8d06d5524ae5851d4a65e28c1bc48
AKIMITSU Taichi <aki32.png@gmail.com>
2023-02-12 20:59:41 +0900

    added

---
+++ 1 Console/100 General/A - ChainableExtensions/B - FileSystem/B104 Read.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Aki32Utilities.ConsoleAppUtilities.General;
+public static partial class ChainableExtensions
+{
+
+    // ★★★★★★★★★★★★★★★ sugar
+
+    public static byte[] ReadAllBytes(this FileInfo inputFile) => File.ReadAllBytes(inputFile.FullName);
+    public static string[] ReadAllLines(this FileInfo inputFile) => File.ReadAllLines(inputFile.FullName);
+    public static string ReadAllText(this FileInfo inputFile) => File.ReadAllText(inputFile.FullName);
+    public static IEnumerable<string> ReadLines(this FileInfo inputFile) => File.ReadLines(inputFile.FullName);
+
+
+    // ★★★★★★★★★★★★★★★ 
+
+}
