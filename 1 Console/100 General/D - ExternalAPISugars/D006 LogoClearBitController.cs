7e47382fbf3acd4ff1cffb764269e9b95bc7b4ea
AKIMITSU Taichi <aki32.png@gmail.com>
2023-02-12 13:29:30 +0900

    added logo downloader

---
+++ 1 Console/100 General/D - ExternalAPISugars/D006 LogoClearBitController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+
+using Aki32Utilities.ConsoleAppUtilities.General;
+
+namespace Aki32Utilities.ConsoleAppUtilities.General;
+public class LogoClearBitController
+{
+
+    // ★★★★★★★★★★★★★★★ props
+
+    public static Uri TargetUri(string targetSite) => new($"https://logo.clearbit.com/{targetSite}");
+
+
+    // ★★★★★★★★★★★★★★★ methods
+
+    /// <summary>
+    /// download logo file from https://logo.clearbit.com
+    /// </summary>
+    public static async Task<bool> DownloadWebSiteLogoAsync(Uri targetSite, FileInfo outputFile)
+    {
+        try
+        {
+            // request
+            await TargetUri(targetSite.ToString())
+                .DownloadFileAsync(outputFile);
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    // ★★★★★★★★★★★★★★★ 
+
+}
