391ace9e8bcb23ef3f0e7647ef5931cb8de30142
AKIMITSU Taichi <aki32.png@gmail.com>
2023-03-03 15:40:49 +0900

    updated dif

--- 1 Console/100 General/B - UsefulClasses/B008 EnumerableExtension.cs
+++ 1 Console/100 General/B - UsefulClasses/B008 EnumerableExtension.cs
@@ -1,27 +1,36 @@
-﻿using System.Drawing;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-using OpenCvSharp.Internal;
+﻿
 
 namespace Aki32Utilities.ConsoleAppUtilities.General;
-
-/// <summary>
-/// Mainly, syntax sugar of device IO OS commands.
-/// </summary>
 public class EnumerableExtension
 {
     // ★★★★★★★★★★★★★★★ for use
 
     /// <summary>
     /// 
-    /// </code>
-    public static IEnumerable<double> Range(double from, double to, int count)
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    public static IEnumerable<double> Range_WithCount(double from, double to, int count)
     {
         return Enumerable
             .Range(0, count)
-            .Select(x => x * (to - from) / (count-1) + from)
+            .Select(x => x * (to - from) / (count - 1) + from)
             ;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    public static IEnumerable<double> Range_WithStep(double from, double to, double step)
+    {
+        var count = (int)((to - from) / step);
+        return Range_WithCount(from, to, count);
     }
 
 
