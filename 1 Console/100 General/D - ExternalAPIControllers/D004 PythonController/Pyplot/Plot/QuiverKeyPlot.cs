56007677a65f50826a2cfc0a81205d1b86014e75
AKIMITSU Taichi <aki32.png@gmail.com>
2023-03-11 20:31:36 +0900

    added QuiverKeyPlot

---
+++ 1 Console/100 General/D - ExternalAPIControllers/D004 PythonController/Pyplot/Plot/QuiverKeyPlot.cs
@@ -0,0 +1,48 @@
+﻿
+
+using DocumentFormat.OpenXml.Office.LongProperties;
+
+using System.Reflection.Emit;
+
+using XPlot.Plotly;
+
+namespace Aki32Utilities.ConsoleAppUtilities.General;
+public static partial class PythonController
+{
+    public partial class PyPlot
+    {
+        /// <summary>
+        /// 3D
+        /// </summary>
+        public class QuiverKeyPlot : IPlot
+        {
+            // ★★★★★★★★★★★★★★★ props
+
+            public bool Is3D { get; set; } = true;
+            public string LegendLabel { get; set; } = "";
+            public double Alpha { get; set; } = 1;
+
+
+            // ★★★★★★★★★★★★★★★ inits
+
+
+            // ★★★★★★★★★★★★★★★ methods
+
+            public void Run(dynamic ax)
+            {
+                // プロット
+                // TODO
+                throw new NotImplementedException();
+
+                //# ベクトルにQuiverのテキストを添える
+                // ax.quiverkey(q, X = 0.75, Y = 1.009, U = 25, label = 'Quiver key, length=1', labelpos = 'E',
+                //fontproperties ={ 'size': 13})
+
+            }
+
+
+            // ★★★★★★★★★★★★★★★ 
+
+        }
+    }
+}
