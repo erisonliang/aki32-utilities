8c18f7721542d898f6cf5699efeb7b586d0ac799
AKIMITSU Taichi <aki32.png@gmail.com>
2023-03-11 20:31:23 +0900

    added TextPlot

---
+++ 1 Console/100 General/D - ExternalAPIControllers/D004 PythonController/Pyplot/Plot/TextPlot.cs
@@ -0,0 +1,57 @@
+﻿
+
+namespace Aki32Utilities.ConsoleAppUtilities.General;
+public static partial class PythonController
+{
+    public partial class PyPlot
+    {
+        /// <summary>
+        /// 2D / 3D
+        /// </summary>
+        public class TextPlot : IPlot
+        {
+            // ★★★★★★★★★★★★★★★ props
+
+            public bool Is3D { get; set; } = true;
+            public string LegendLabel { get; set; } = "";
+            public double Alpha { get; set; } = 1;
+
+            public double X { get; set; }
+            public double Y { get; set; }
+            /// <summary>
+            /// (3d option)
+            /// </summary>
+            public string Text { get; set; }
+
+            public string? Color { get; set; } = null;
+            public int? FontSize { get; set; } = null;
+
+
+            // ★★★★★★★★★★★★★★★ inits
+
+            public TextPlot(double x, double y, string text)
+            {
+                X = x;
+                Y = y;
+                Text = text;
+                Is3D = false;
+            }
+
+
+            // ★★★★★★★★★★★★★★★ methods
+
+            public void Run(dynamic ax)
+            {
+                // プロット
+                ax.text(X, Y, Text,
+                    color: Color,
+                    size: FontSize
+                    );
+            }
+
+
+            // ★★★★★★★★★★★★★★★ 
+
+        }
+    }
+}
