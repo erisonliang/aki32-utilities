580a54c6fb23c3027e5591824c91272f95ec7209
AKIMITSU Taichi <aki32.png@gmail.com>
2023-03-11 17:52:22 +0900

    added BarPlot

--- 1 Console/100 General/D - ExternalAPIControllers/D004 PythonController/Pyplot/Plot/BarPlot.cs
+++ 1 Console/100 General/D - ExternalAPIControllers/D004 PythonController/Pyplot/Plot/BarPlot.cs
@@ -6,9 +6,9 @@ public static partial class PythonController
     public partial class PyPlot
     {
         /// <summary>
-        /// 2D / 3D
+        /// 2D
         /// </summary>
-        public class BarPlot : IPlot
+        public class BarPlot<T_XLabel> : IPlot
         {
             // ★★★★★★★★★★★★★★★ props
 
@@ -16,8 +16,23 @@ public static partial class PythonController
             public string LegendLabel { get; set; } = "";
             public double Alpha { get; set; } = 1;
 
+            public object[] X { get; set; }
+            public double[] Height { get; set; }
+
+            public double[]? Bottoms { get; set; } = null;
+            public double Width { get; set; } = 0.8;
+            public double? LineWidth { get; set; } = null;
+
+            public string Alignment { get; set; } = "center";
 
             // ★★★★★★★★★★★★★★★ inits
+
+            public BarPlot(T_XLabel[]? x, double[] height)
+            {
+                X = x?.Select(o => (object)o!)!.ToArray()!;
+                Height = height;
+                Is3D = false;
+            }
 
 
             // ★★★★★★★★★★★★★★★ methods
@@ -25,8 +40,17 @@ public static partial class PythonController
             public void Run(dynamic ax)
             {
                 // プロット
-                // TODO Bar
-                throw new NotImplementedException();
+                ax.bar(X, Height,
+                      label: LegendLabel,
+                      alpha: Alpha,
+
+                      linewidth: LineWidth,
+                      width: Width,
+                      bottom: Bottoms,
+
+                      align: Alignment
+                );
+
             }
 
 
