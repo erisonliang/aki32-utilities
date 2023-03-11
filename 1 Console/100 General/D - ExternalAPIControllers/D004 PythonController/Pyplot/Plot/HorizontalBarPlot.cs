79c5c1be5b7cd310a21287e6a112811b4a1b3976
AKIMITSU Taichi <aki32.png@gmail.com>
2023-03-11 12:54:53 +0900

    added HorizontalBarPlot

---
+++ 1 Console/100 General/D - ExternalAPIControllers/D004 PythonController/Pyplot/Plot/HorizontalBarPlot.cs
@@ -0,0 +1,67 @@
+﻿
+
+using DocumentFormat.OpenXml.Drawing.Charts;
+
+using XPlot.Plotly;
+
+namespace Aki32Utilities.ConsoleAppUtilities.General;
+public static partial class PythonController
+{
+    public partial class PyPlot
+    {
+        /// <summary>
+        /// 2D
+        /// </summary>
+        public class HorizontalBarPlot<T_YLabel> : IPlot
+        {
+            // ★★★★★★★★★★★★★★★ props
+
+            public bool Is3D { get; set; } = false;
+            public string LegendLabel { get; set; } = "";
+            public double Alpha { get; set; } = 1;
+
+            public double[] X { get; set; }
+            public object[] Y { get; set; }
+
+            public double[]? Lefts { get; set; } = null;
+            public double? Height { get; set; } = null;
+            public double? LineWidth { get; set; } = null;
+
+            public string Alignment { get; set; } = "center";
+
+            // ★★★★★★★★★★★★★★★ inits
+
+            public HorizontalBarPlot(T_YLabel[]? y, double[] x)
+            {
+                X = x;
+                Y = y?.Select(o => (object)o!)!.ToArray()!;
+                Is3D = false;
+            }
+
+
+            // ★★★★★★★★★★★★★★★ methods
+
+            public void Run(dynamic ax)
+            {
+                // プロット
+                ax.barh(Y, X,
+                      label: LegendLabel,
+                      alpha: Alpha,
+
+                      linewidth: LineWidth,
+                      height: Height,
+                      left: Lefts,
+
+                      align: Alignment
+                );
+
+                // 残り barh(   fc, ec, xerr, capsize, log)
+
+            }
+
+
+            // ★★★★★★★★★★★★★★★ 
+
+        }
+    }
+}
