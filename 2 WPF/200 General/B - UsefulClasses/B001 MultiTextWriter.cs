5c09c2045562cf5234f20570d65ef83ae093fb8d
AKIMITSU Taichi <aki32.png@gmail.com>
2023-04-03 18:36:02 +0900

    added MultiTextWriter

---
+++ 2 WPF/200 General/B - UsefulClasses/B001 MultiTextWriter.cs
@@ -0,0 +1,98 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows.Markup;
+
+namespace Aki32Utilities.WPFAppUtilities.General;
+public class ControlWriter : TextWriter
+{
+
+    // ★★★★★★★★★★★★★★★ props
+
+    private IAddChild control;
+
+
+    // ★★★★★★★★★★★★★★★ inits
+
+    public ControlWriter(IAddChild control)
+    {
+        this.control = control;
+    }
+
+
+    // ★★★★★★★★★★★★★★★ methods
+
+    public override void Write(char value)
+    {
+        control.AddText(value.ToString());
+    }
+
+    public override void Write(string value)
+    {
+        control.AddText(value);
+    }
+
+    public override Encoding Encoding
+    {
+        get { return Encoding.ASCII; }
+    }
+
+
+    // ★★★★★★★★★★★★★★★
+
+}
+
+public class MultiTextWriter : TextWriter
+{
+
+    // ★★★★★★★★★★★★★★★ props
+
+    private IEnumerable<TextWriter> writers;
+
+
+    // ★★★★★★★★★★★★★★★ inits
+
+    public MultiTextWriter(IEnumerable<TextWriter> writers)
+    {
+        this.writers = writers.ToList();
+    }
+    public MultiTextWriter(params TextWriter[] writers)
+    {
+        this.writers = writers;
+    }
+
+
+    // ★★★★★★★★★★★★★★★ methods
+
+    public override void Write(char value)
+    {
+        foreach (var writer in writers)
+            writer.Write(value);
+    }
+
+    public override void Write(string value)
+    {
+        foreach (var writer in writers)
+            writer.Write(value);
+    }
+
+    public override void Flush()
+    {
+        foreach (var writer in writers)
+            writer.Flush();
+    }
+
+    public override void Close()
+    {
+        foreach (var writer in writers)
+            writer.Close();
+    }
+
+    public override Encoding Encoding
+    {
+        get { return Encoding.ASCII; }
+    }
+
+
+    // ★★★★★★★★★★★★★★★
+
+}
