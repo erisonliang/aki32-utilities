bee7e160f8a2b4aab298dbbfc260df7eb74312b4
AKIMITSU Taichi <aki32.png@gmail.com>
2023-04-14 21:35:14 +0900

    added new NullOrEmpty

---
+++ 1 Console/100_General/A - ChainableExtensions/I - String/I208 IsNullOrEmpty.cs
@@ -0,0 +1,23 @@
+﻿using System.IO.Compression;
+using System.Text;
+
+namespace Aki32Utilities.ConsoleAppUtilities.General;
+public static partial class ChainableExtensions
+{
+
+    public static bool IsNullOrEmpty(this string? input)
+    {
+        return string.IsNullOrEmpty(input);
+    }
+
+    public static bool IsNullOrWhiteSpace(this string? input)
+    {
+        return string.IsNullOrWhiteSpace(input);
+    }
+
+    public static bool IsNullOrWhiteSpaceOrNewLine(this string? input)
+    {
+        return input.IsNullOrWhiteSpace() || input!.Replace("\r", "").Replace("\n", "").IsNullOrWhiteSpace();
+    }
+
+}
