db6b54c6afd1ba512e0f94f89b566ece9e895c97
aki32 <aki32.png@gmail.com>
2023-04-17 21:58:17 +0900

    added

---
+++ 1_Console/100_General/A_ChainableExtensions/I_String/I209_ReplaceUnwantedCharacters.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Aki32Utilities.ConsoleAppUtilities.General;
+public static partial class ChainableExtensions
+{
+
+    /// <summary>
+    /// generate hash
+    /// </summary>
+    /// <param name="inputPath"></param>
+    /// <param name="newFileNameWithoutExtension">"*" will be replaced with old name</param>
+    /// <param name="replaceSets">replace strings in original file name</param>
+    /// <returns></returns>
+    public static string ReplaceUnwantedCharacters(this string inputString)
+    {
+        // main
+        return inputString
+            .Replace("\u00a0", " ")
+            ;
+    }
+
+}
