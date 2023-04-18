5469550bc872750f1a05ffd396a07ab2df6ba68c
AKIMITSU Taichi <aki32.png@gmail.com>
2023-04-18 17:42:47 +0900

    added WFA Control project

---
+++ 3_WFA/301_Control/ZoomableImage.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Aki32Utilities.WFAAppUtilities.Control
+{
+    partial class ZoomableImage
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainPictureBox
+            // 
+            this.MainPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.MainPictureBox.Name = "MainPictureBox";
+            this.MainPictureBox.Size = new System.Drawing.Size(800, 450);
+            this.MainPictureBox.TabIndex = 0;
+            this.MainPictureBox.TabStop = false;
+            this.MainPictureBox.SizeChanged += new System.EventHandler(this.MainPictureBox_SizeChanged);
+            this.MainPictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainPictureBox_MouseDown);
+            this.MainPictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainPictureBox_MouseMove);
+            this.MainPictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MainPictureBox_MouseUp);
+            // 
+            // ZoomableImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.MainPictureBox);
+            this.Name = "ZoomableImage";
+            this.Size = new System.Drawing.Size(800, 450);
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MainPictureBox;
+    }
+}
---
+++ 3_WFA/301_Control/ZoomableImage.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace Aki32Utilities.WFAAppUtilities.Control
+{
+    public partial class ZoomableImage : UserControl
+    {
+
+        // ★★★★★★★★★★★★★★★ props
+
+        public FileInfo ImageLocation
+        {
+            set => Image = new Bitmap(value.FullName);
+        }
+        public Bitmap Image
+        {
+            get => bmp;
+            set => InitDrawImage(value);
+        }
+
+
+        // ★★★★★★★★★★★★★★★ fields
+
+        /// <summary>
+        /// 表示するBitmap
+        /// </summary>
+        private Bitmap bmp = null;
+        /// <summary>
+        /// 描画用Graphicsオブジェクト
+        /// </summary>
+        private Graphics g = null;
+        /// <summary>
+        /// マウスダウンフラグ
+        /// </summary>
+        private bool MouseDownFlg = false;
+        /// <summary>
+        /// マウスをクリックした位置の保持用
+        /// </summary>
+        private PointF OldPoint;
+        /// <summary>
+        /// アフィン変換行列
+        /// </summary>
+        private Matrix mat;
+
+
+        // ★★★★★★★★★★★★★★★ inits
+
+        public ZoomableImage()
+        {
+            InitializeComponent();
+
+            MainPictureBox.MouseWheel += new MouseEventHandler(this.MainPictureBox_MouseWheel);
+
+            // 強制的に実行。Graphicsオブジェクトの作成のため。
+            MainPictureBox_SizeChanged(null, null);
+        }
+
+
+        // ★★★★★★★★★★★★★★★ events
+
+        private void MainPictureBox_SizeChanged(object sender, EventArgs e)
+        {
+            if (g != null)
+            {
+                mat = g.Transform;
+                g.Dispose();
+                g = null;
+            }
+
+            // PictureBoxと同じ大きさのBitmapクラスを作成する。
+            Bitmap bmpPicBox = new Bitmap(MainPictureBox.Width, MainPictureBox.Height);
+            // 空のBitmapをPictureBoxのImageに指定する。
+            MainPictureBox.Image = bmpPicBox;
+            // Graphicsオブジェクトの作成(FromImageを使う)
+            g = Graphics.FromImage(MainPictureBox.Image);
+            // アフィン変換行列の設定
+            if (mat != null)
+            {
+                g.Transform = mat;
+            }
+
+            // 補間モードの設定（このサンプルではNearestNeighborに設定）
+            g.InterpolationMode
+                = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+            // 画像の描画
+            RedrawImage();
+        }
+
+        private void MainPictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            // 右ボタンがクリックされたとき
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                // アフィン変換行列に単位行列を設定する
+                mat.Reset();
+                // 画像の描画
+                RedrawImage();
+
+                return;
+            }
+            // フォーカスの設定
+            //（クリックしただけではMouseWheelイベントが有効にならない）
+            MainPictureBox.Focus();
+            // マウスをクリックした位置の記録
+            OldPoint.X = e.X;
+            OldPoint.Y = e.Y;
+            // マウスダウンフラグ
+            MouseDownFlg = true;
+        }
+
+        private void MainPictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            // マウスをクリックしながら移動中のとき
+            if (MouseDownFlg == true)
+            {
+                // 画像の移動
+                mat.Translate(e.X - OldPoint.X, e.Y - OldPoint.Y,
+                    System.Drawing.Drawing2D.MatrixOrder.Append);
+                // 画像の描画
+                RedrawImage();
+
+                // ポインタ位置の保持
+                OldPoint.X = e.X;
+                OldPoint.Y = e.Y;
+            }
+        }
+
+        private void MainPictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            // マウスダウンフラグ
+            MouseDownFlg = false;
+        }
+
+        private void MainPictureBox_MouseWheel(object sender, MouseEventArgs e)
+        {
+            // ポインタの位置→原点へ移動
+            mat.Translate(-e.X, -e.Y,
+                System.Drawing.Drawing2D.MatrixOrder.Append);
+            if (e.Delta > 0)
+            {
+                // 拡大
+                if (mat.Elements[0] < 100)  // X方向の倍率を代表してチェック
+                {
+                    mat.Scale(1.5f, 1.5f,
+                        System.Drawing.Drawing2D.MatrixOrder.Append);
+                }
+            }
+            else
+            {
+                // 縮小
+                if (mat.Elements[0] > 0.01)  // X方向の倍率を代表してチェック
+                {
+                    mat.Scale(1.0f / 1.5f, 1.0f / 1.5f,
+                        System.Drawing.Drawing2D.MatrixOrder.Append);
+                }
+            }
+            // 原点→ポインタの位置へ移動(元の位置へ戻す)
+            mat.Translate(e.X, e.Y,
+                System.Drawing.Drawing2D.MatrixOrder.Append);
+            // 画像の描画
+            RedrawImage();
+        }
+
+
+        // ★★★★★★★★★★★★★★★ methods
+
+        /// <summary>
+        /// Bitmap初期描画
+        /// </summary>
+        private void InitDrawImage(Bitmap bitmap)
+        {
+            bmp?.Dispose();
+            bmp = bitmap;
+
+            mat?.Dispose();
+            mat = new Matrix();
+
+            RedrawImage();
+        }
+
+        /// <summary>
+        /// Bitmap再描画
+        /// </summary>
+        private void RedrawImage()
+        {
+            if (bmp == null)
+                return;
+
+            if (mat != null)
+                g.Transform = mat;
+
+            g.Clear(MainPictureBox.BackColor);
+            g.DrawImage(bmp, 0, 0);
+
+            MainPictureBox.Refresh();
+        }
+
+
+        // ★★★★★★★★★★★★★★★ 
+
+    }
+}
