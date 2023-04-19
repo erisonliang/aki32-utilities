22c2d298b35f663fd2b041f4fbc874904212db6d
AKIMITSU Taichi <aki32.png@gmail.com>
2023-04-19 21:18:23 +0900

    updated FFT

--- A_Console/A10_StructuralEngineering/A_ChainableExtensions/F_TimeHistory/F203_FFT.cs
+++ A_Console/A10_StructuralEngineering/A_ChainableExtensions/F_TimeHistory/F203_FFT.cs
@@ -1,6 +1,4 @@
-﻿using System.Numerics;
-using MathNet.Numerics.IntegralTransforms;
-using Aki32Utilities.ConsoleAppUtilities.General;
+﻿using Aki32Utilities.ConsoleAppUtilities.General;
 
 namespace Aki32Utilities.ConsoleAppUtilities.StructuralEngineering;
 public static partial class ChainableExtensions
@@ -8,53 +6,22 @@ public static partial class ChainableExtensions
 
     public static async Task<TimeHistory> FFT(this TimeHistory inputHistory, string targetIndex)
     {
-        var inputHistoryClone = inputHistory.Clone();
-        var resultHistory = inputHistory.Clone();
-        resultHistory.Name += "_FFT";
-        resultHistory.DropAllColumns();
-
-        if (inputHistoryClone.TimeStep == 0)
+        // preprocess
+        if (!inputHistory.Columns.Contains(targetIndex))
+            throw new InvalidDataException($"\"{targetIndex}\" index (designated by \"targetIndex\") is required in inputHistory");
+        if (!inputHistory.Columns.Contains("t"))
             throw new InvalidDataException("\"t\" index is required in inputHistory");
-        var sampleRate = 1d / inputHistoryClone.TimeStep;
+        if (inputHistory.TimeStep == 0)
+            throw new InvalidDataException("\"t\" have to be in correct format.");
 
-        await Task.Run(() =>
-        {
-            // 2の累乗にカットして複素数データに変換
-            double len = inputHistoryClone.DataRowCount;
-            int Np = 0;
-            while (len > 1)
-            {
-                Np++;
-                len /= 2;
-            }
-            Np--;
 
-            if (Np == 0)
-                return;
-            int N = (int)Math.Pow(2, Np); // サンプル数
+        // main
+        var resultHistory = await FFTExecuter.Execute(inputHistory.TimeStep, inputHistory[targetIndex]);
 
-            var complexVoice = inputHistoryClone[targetIndex]
-                .Take(N)
-                .Select(x => new Complex(x, 0))
-                .ToArray();
 
-            // FFTを実行
-            Fourier.Forward(complexVoice, FourierOptions.Default);
-
-            // グラフのデータを変換。
-            var freqStep = sampleRate / N;
-
-            for (int i = 0; i < N / 2; i++)
-            {
-                var step = new TimeHistoryStep();
-                var freq = freqStep * i;
-                step["Freq"] = freq;
-                step["T"] = freq == 0 ? double.PositiveInfinity : 1d / freq;
-                step["Amp"] = Math.Pow(complexVoice[i].Magnitude, 2);
-                resultHistory.AppendStep(step);
-            }
-        });
-
+        // post process
+        resultHistory.inputDir = inputHistory.inputDir;
+        resultHistory.Name = $"{inputHistory.Name}_FFT";
         return resultHistory;
     }
 
---
+++ A_Console/A10_StructuralEngineering/C_AwesomeModels/B_Dynamics/B005_FFT/FFTExecuter.cs
@@ -0,0 +1,47 @@
+﻿using System.Numerics;
+
+using Aki32Utilities.ConsoleAppUtilities.General;
+
+using MathNet.Numerics.IntegralTransforms;
+
+namespace Aki32Utilities.ConsoleAppUtilities.StructuralEngineering;
+public class FFTExecuter
+{
+    /// <summary>
+    /// Fast Fourier Transform
+    /// </summary>
+    /// <param name="inputData"></param>
+    public static async Task<TimeHistory> Execute(double timeStep, double[] inputData)
+    {
+        if (timeStep == 0)
+            throw new InvalidDataException("timeStep must be more than 0.");
+        var sampleRate = 1d / timeStep;
+        var resultHistory = new TimeHistory();
+
+        await Task.Run(() =>
+        {
+            // 2の累乗にカットして複素数データに変換
+            var cutData = DynamicsUtils.MakePow2Order(inputData, true, out int N);
+            var complexInputData = cutData.Select(x => new Complex(x, 0)).ToArray();
+
+            // FFTを実行
+            Fourier.Forward(complexInputData, FourierOptions.Default);
+
+            // グラフのデータを変換。
+            var freqStep = sampleRate / N;
+
+            for (int i = 0; i < N / 2; i++)
+            {
+                var step = new TimeHistoryStep();
+                var freq = freqStep * i;
+                step["Freq"] = freq;
+                step["T"] = freq == 0 ? double.PositiveInfinity : 1d / freq;
+                step["Amp"] = Math.Pow(complexInputData[i].Magnitude, 2);
+                resultHistory.AppendStep(step);
+            }
+
+        });
+
+        return resultHistory;
+    }
+}
