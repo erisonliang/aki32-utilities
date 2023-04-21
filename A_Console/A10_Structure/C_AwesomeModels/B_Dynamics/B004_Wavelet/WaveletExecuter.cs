c6363057ccdefcc7c808dfd1b5b723696abeee20
AKIMITSU Taichi <aki32.png@gmail.com>
2023-04-21 16:07:21 +0900

    updated coefs

--- A_Console/A10_Structure/C_AwesomeModels/B_Dynamics/B004_Wavelet/WaveletExecuter.cs
+++ A_Console/A10_Structure/C_AwesomeModels/B_Dynamics/B004_Wavelet/WaveletExecuter.cs
@@ -6,18 +6,26 @@ namespace Aki32Utilities.ConsoleAppUtilities.Structure;
 public class WaveletExecuter
 {
     /// <summary>
-    /// Daubechies係数
+    /// Daubechies係数 (*√2)
     /// </summary>
     /// <see cref="https://en.wikipedia.org/wiki/Daubechies_wavelet#The_scaling_sequences_of_lowest_approximation_order"/>
-    private static DenseMatrix DaubechiesCoefficients => (DenseMatrix)(new DenseMatrix(10, 5, new double[]
+    private static double[][] DaubechiesCoefficientsBaseArray => new double[][]
     {
-        1,1,0,0,0,0,0,0,0,0,
-        0.6830127,1.1830127,0.3169873,-0.1830127,0,0,0,0,0,0,
-        0.47046721,1.14111692,0.650365,-0.19093442,-0.12083221,0.0498175,0,0,0,0,
-        0.32580343,1.01094572,0.8922014,-0.03957503, -0.26450717,0.0436163,0.0465036,-0.01498699, 0,0,
-        0.22641898,0.85394354,1.02432694,0.19576696, -0.34265671,-0.04560113,0.10970265,-0.0088268, -0.01779187,4.72E-03
-    }) / Math.Sqrt(2));
-    private static int[] AcceptingD = new int[] { 2, 4, 6, 8, 10 };
+        new double[]{ 1, 1 },
+        new double[]{ 0.6830127,1.1830127,0.3169873,-0.1830127 },
+        new double[]{ 0.47046721,1.14111692,0.650365,-0.19093442,-0.12083221,0.0498175 },
+        new double[]{ 0.32580343,1.01094572,0.8922014,-0.03957503, -0.26450717,0.0436163,0.0465036,-0.01498699 },
+        new double[]{ 0.22641898,0.85394354,1.02432694,0.19576696, -0.34265671,-0.04560113,0.10970265,-0.0088268, -0.01779187,4.72E-03 },
+    };
+    /// <summary>
+    /// Daubechies係数
+    /// </summary>
+    private static DenseMatrix DaubechiesCoefficientsMatrix
+        => (DenseMatrix)(new DenseMatrix(10, 5, DaubechiesCoefficientsBaseArray.ConvertTo2DArray().ReShape()) / Math.Sqrt(2));
+    /// <summary>
+    /// 引数Dに入力できる数値
+    /// </summary>
+    private static readonly int[] AcceptingD = new int[] { 2, 4, 6, 8, 10 };
 
     /// <summary>
     /// DaubechiesWavelet変換。
@@ -32,7 +40,7 @@ public class WaveletExecuter
         // ★ 諸元
         if (!AcceptingD.Contains(D))
             throw new Exception($"D must be any of ({string.Join(", ", AcceptingD)})");
-        var usingDaubechiesCoefficients = DaubechiesCoefficients.Column(D / 2 - 1);
+        var usingDaubechiesCoefficients = DaubechiesCoefficientsMatrix.Column(D / 2 - 1);
 
         var extendedInputData = DynamicsUtils.MakeDataLengthPow2(inputData, false, out int totalLength);
         var resultData = new double[totalLength];
