f825c86d931fcc16be29e3cb8ad8956867d5365a
AKIMITSU Taichi <aki32.png@gmail.com>
2023-04-23 06:01:17 +0900

    updated to static kernel generater

---
+++ A_Console/A01_PythonAndNumerics/C_AwesomeModels/3_Matrix/3001_GaussianProcessRegression/Kernel/10_GeneralKernels.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Aki32Utilities.ConsoleAppUtilities.PythonAndNumerics;
+public partial class GaussianProcessRegressionExecuter
+{
+    public static class GeneralKernels
+    {
+        // ★★★★★★★★★★★★★★★ methods (static)
+
+        /// <summary>
+        /// Super basic one
+        /// </summary>
+        public static KernelBase GeneralKernel(double constantWeight, double lengthScale, double noiseLambda)
+            => new ConstantKernel(constantWeight) * new SquaredExponentialKernel(lengthScale) + new WhiteNoiseKernel(noiseLambda);
+
+
+        // ★★★★★★★★★★★★★★★ 
+
+    }
+}
