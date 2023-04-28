a387d11e955f088dae1e98eb8014c918c4556191
AKIMITSU Taichi <aki32.png@gmail.com>
2023-04-28 11:40:33 +0900

    update vars

--- A_Console/A01_PythonAndNumerics/C_AwesomeModels/3_Matrix/3001_GaussianProcessRegression/GaussianProcessRegressionExecuter.cs
+++ A_Console/A01_PythonAndNumerics/C_AwesomeModels/3_Matrix/3001_GaussianProcessRegression/GaussianProcessRegressionExecuter.cs
@@ -132,7 +132,7 @@ public partial class GaussianProcessRegressionExecuter
             if (executePreciseOptimization)
             {
                 using var progress = new ProgressManager(maxTryCount);
-                Console.WriteLine("precise optimize");
+                Console.WriteLine("precise optimization");
                 progress.StartAutoWrite();
                 var signSwap = 1d; //for emergency
 
@@ -235,10 +235,10 @@ public partial class GaussianProcessRegressionExecuter
 
 
         // fit and predict
-        (var Y_predict, var Y_Cov) = gpr.FitAndPredict(X_train, Y_train, X_predict);
-        var Y_Std = Y_Cov.Select(x => Math.Sqrt(x)).ToArray();
-        var Y_95CI = Y_Std.ProductForEach(1.96);
-        var Y_99CI = Y_Std.ProductForEach(2.58);
+        (var Y_predict, var Y_cov) = gpr.FitAndPredict(X_train, Y_train, X_predict);
+        var Y_std = Y_cov.Select(x => Math.Sqrt(x)).ToArray();
+        var Y_95CI = Y_std.ProductForEach(1.96);
+        var Y_99CI = Y_std.ProductForEach(2.58);
 
 
         // draw
